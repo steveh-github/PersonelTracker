0af31ded575118527aafa8787865ddc74647ec11
Stephen Hall <stephenhall@gmail.com>
2021-07-02 14:07:29 +0100

    work on logon form

--- PersonalTracking/FrmLogin.Designer.cs
+++ PersonalTracking/FrmLogin.Designer.cs
@@ -33,15 +33,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(152, 151);
+            this.button1.Location = new System.Drawing.Point(209, 125);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 52);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Exit";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // txtUserNo
@@ -59,7 +60,7 @@
             this.label1.Location = new System.Drawing.Point(22, 40);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 17);
-            this.label1.TabIndex = 2;
+            this.label1.TabIndex = 0;
             this.label1.Text = "User Number";
             // 
             // label2
@@ -68,7 +69,7 @@
             this.label2.Location = new System.Drawing.Point(22, 71);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 17);
-            this.label2.TabIndex = 4;
+            this.label2.TabIndex = 2;
             this.label2.Text = "Password";
             // 
             // txtPassword
@@ -78,11 +79,21 @@
             this.txtPassword.Size = new System.Drawing.Size(164, 22);
             this.txtPassword.TabIndex = 3;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(121, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 52);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(319, 200);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label1);
@@ -102,6 +113,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ PersonalTracking/frmMain.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PersonalTracking
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmployee = new System.Windows.Forms.Button();
+            this.txtTasks = new System.Windows.Forms.Button();
+            this.txtSalary = new System.Windows.Forms.Button();
+            this.txtPermissions = new System.Windows.Forms.Button();
+            this.txtDepartment = new System.Windows.Forms.Button();
+            this.txtPosition = new System.Windows.Forms.Button();
+            this.txtLogOut = new System.Windows.Forms.Button();
+            this.txtExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEmployee
+            // 
+            this.txtEmployee.Location = new System.Drawing.Point(23, 22);
+            this.txtEmployee.Name = "txtEmployee";
+            this.txtEmployee.Size = new System.Drawing.Size(135, 50);
+            this.txtEmployee.TabIndex = 0;
+            this.txtEmployee.Text = "Employee";
+            this.txtEmployee.UseVisualStyleBackColor = true;
+            // 
+            // txtTasks
+            // 
+            this.txtTasks.Location = new System.Drawing.Point(164, 22);
+            this.txtTasks.Name = "txtTasks";
+            this.txtTasks.Size = new System.Drawing.Size(135, 50);
+            this.txtTasks.TabIndex = 1;
+            this.txtTasks.Text = "Tasks";
+            this.txtTasks.UseVisualStyleBackColor = true;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(305, 22);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(135, 50);
+            this.txtSalary.TabIndex = 2;
+            this.txtSalary.Text = "Salary";
+            this.txtSalary.UseVisualStyleBackColor = true;
+            // 
+            // txtPermissions
+            // 
+            this.txtPermissions.Location = new System.Drawing.Point(23, 87);
+            this.txtPermissions.Name = "txtPermissions";
+            this.txtPermissions.Size = new System.Drawing.Size(135, 50);
+            this.txtPermissions.TabIndex = 3;
+            this.txtPermissions.Text = "Permissons";
+            this.txtPermissions.UseVisualStyleBackColor = true;
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.Location = new System.Drawing.Point(164, 87);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.Size = new System.Drawing.Size(135, 50);
+            this.txtDepartment.TabIndex = 4;
+            this.txtDepartment.Text = "Department";
+            this.txtDepartment.UseVisualStyleBackColor = true;
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.Location = new System.Drawing.Point(305, 87);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(135, 50);
+            this.txtPosition.TabIndex = 5;
+            this.txtPosition.Text = "Positions";
+            this.txtPosition.UseVisualStyleBackColor = true;
+            // 
+            // txtLogOut
+            // 
+            this.txtLogOut.Location = new System.Drawing.Point(164, 166);
+            this.txtLogOut.Name = "txtLogOut";
+            this.txtLogOut.Size = new System.Drawing.Size(135, 50);
+            this.txtLogOut.TabIndex = 6;
+            this.txtLogOut.Text = "Logout";
+            this.txtLogOut.UseVisualStyleBackColor = true;
+            // 
+            // txtExit
+            // 
+            this.txtExit.Location = new System.Drawing.Point(305, 166);
+            this.txtExit.Name = "txtExit";
+            this.txtExit.Size = new System.Drawing.Size(135, 50);
+            this.txtExit.TabIndex = 7;
+            this.txtExit.Text = "Exit";
+            this.txtExit.UseVisualStyleBackColor = true;
+            this.txtExit.Click += new System.EventHandler(this.txtExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 232);
+            this.Controls.Add(this.txtExit);
+            this.Controls.Add(this.txtLogOut);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.txtDepartment);
+            this.Controls.Add(this.txtPermissions);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtTasks);
+            this.Controls.Add(this.txtEmployee);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "frmMain";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button txtEmployee;
+        private System.Windows.Forms.Button txtTasks;
+        private System.Windows.Forms.Button txtSalary;
+        private System.Windows.Forms.Button txtPermissions;
+        private System.Windows.Forms.Button txtDepartment;
+        private System.Windows.Forms.Button txtPosition;
+        private System.Windows.Forms.Button txtLogOut;
+        private System.Windows.Forms.Button txtExit;
+    }
+}
